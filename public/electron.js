97ff938a300bfec3c6f0e93484cbfdb88ba1ef83
Jeremy Boggs <nicocchilover@gmail.com>
2022-03-02 12:12:39 -0500

    revised the main electron file and packages used

--- public/electron.js
+++ public/electron.js
@@ -1,100 +1,87 @@
-require('v8-compile-cache');
-const { app, BrowserWindow, ipcMain} = require("electron");
-const electron = require('electron')
-
-const _HOME_ = require("os").homedir();
-const _SEP_ = require("path").sep;
-const _APPHOME_ = `${_HOME_}${_SEP_}.ingenium${_SEP_}`;
-
+const { app, BrowserWindow, ipcMain, Menu } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev"); // To check if electron is running in dev mode or not
 
 // The main window
 let mainWindow = null;
 
-// windows[i] = new BrowserWindow({
-//     width: displays[i].bounds.width,
-//     height: displays[i].bounds.height,
-//     x: displays[i].bounds.x,
-//     y: displays[i].bounds.y,
-//     resizable: false,
-//     transparent: false,
-//     fullscreen: true,
-//     movable: false,
-//     skipTaskbar: true,
-//     type: "desktop",
-//     useContentSize: true,
-//     icon: `${__dirname}/icon.png`,
-//     webPreferences: {
-//         preload: path.join(__dirname, "preload.js"),
-//         nodeIntegrationInWorker: true,
-//         webSecurity: true,
-//         backgroundThrottling: false
-//     }
-// });
+// Template application menu
+const menuTemplate = [
+	{ id: '1', label: 'one' },
+	{ type: 'separator' },
+	{ id: '3', label: 'three', beforeGroupContaining: ['1'] },
+	{ id: '4', label: 'four', afterGroupContaining: ['2'] },
+	{ type: 'separator' },
+	{ id: '2', label: 'two' }
+  ]
 
 /**
- * Create the main window
+ * Creates the main window of the application
  *
  */
 function createMainWindow() {
-	const isDev = require("electron-is-dev");
-	const path = require("path");
-	// let bounds = electron.screen.getPrimaryDisplay().bounds;
-	// let x = Math.ceil(bounds.x + ((bounds.width - 1200) / 2));
-	// let y = Math.ceil(bounds.y + ((bounds.height - 800) / 2));
-
-	const {width, height} = electron.screen.getPrimaryDisplay().workAreaSize
-	let x = width / 4
-	let y = height / 4
 	mainWindow = new BrowserWindow({
-		width: 1200,
-		height: 800,
-		resizable: false,
-		x: x,
-		y: y,
-		center: true,
-		icon: `${__dirname}/icon.png`,
+		
+		width: 1200, 						// Integer - Window's width in pixels
+		height: 800, 						// Integer - Window's height in pixels
+		resizable: true, 					// Boolean - Whether window is resizable
+		center: true, 						// Boolean - Show window in the center of the screen
+		icon: `${__dirname}/logo192.png`, 		// NativeImage | String - The window icon. 
+												// On Windows it is recommended to use ICO icons to get best visual effects, you can 
+												// also leave it undefined so the executable's icon will be used.
+												// __dirname is the /src/ folder
+											// Settings of web page's features.
 		webPreferences: {
-			nodeIntegration: false,
-			nodeIntegrationInWorker: true,
-			webSecurity: false,
-			preload: path.join(__dirname, "preload.js"),
+			preload: path.join(__dirname, "preload.js"),	// Specifies a script that will be loaded before other scripts run in the page. 
+															// This script will always have access to node APIs no matter whether node integration 
+															// is turned on or off. The value should be the absolute file path to the script. When node 
+															// integration is turned off, the preload script can reintroduce Node global symbols back 
+															// to the global scope
 		}
 	});
 
-	// mainWindow.webContents.openDevTools();
-	mainWindow.setMenu(null);
-	mainWindow.allowRendererProcessReuse = true;
-	// Load React
-	// mainWindow.openDevTools();
+	// Load React. If in development, show the current server (with hotreload) else, show
+	// the built version
 	mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
+
+	// Open the DevTools in "detached" state if in development
+	if (isDev) {
+		mainWindow.webContents.openDevTools({ mode: "detach" });
+	}
+
+	// Set up the application menu
+	const menu = Menu(menuTemplate);
+    Menu.setApplicationMenu(menu);
+
+	// While loading the page, this event will be emitted when the renderer
+	// process has rendered the page for the first time if the window has not
+	// been shown yet. Showing the window after this event will have no visual flash
+	mainWindow.once("ready-to-show", () => {
+		mainWindow.show();
+	})
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on("ready", () => {
-	const { globalShortcut } = require("electron");
 
 	// Create the main window
 	createMainWindow();
-
-	// Register a 'CommandOrControl+X' shortcut listener
-	const ret = globalShortcut.register("CommandOrControl+7", () => {
-		createMainWindow();
-	});
 });
 
+// Emitted before the application starts closing its windows
 app.once("before-quit", () => {
 	window.removeAllListeners("close");
 });
 
 // Quit when all windows are closed.
-app.on("window-all-closed", function() {
+app.on("window-all-closed", function () {
 	// On macOS it is common for applications and their menu bar
 	// to stay active until the user quits explicitly with Cmd + Q
 	if (process.platform !== "darwin") app.quit();
 });
 
-app.on("activate", function() {
+app.on("activate", function () {
 	// On macOS it's common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
 	if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Basic Create-React-App template for Electron.</p>
+        <p>Electron template for Create-React-App.</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
